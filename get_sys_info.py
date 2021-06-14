2e4b61f5b2a8e79df6ed5290eb96101c5e4b4739
Kacper Chołody <cholody.kacper1998@gmail.com>
2021-06-14 17:00:03 +0200

    Added current time

--- get_sys_info.py
+++ get_sys_info.py
@@ -9,4 +9,15 @@ def check_system_type(expected: str) -> bool:
     else:
         return False;
 
-print(check_system_type("Linux"))
+def get_time() -> str:
+    cur_time = time.localtime(time.time())
+
+    ret = f"""{cur_time.tm_mday if (cur_time.tm_mday > 9) else f"0{cur_time.tm_mday}"}/{cur_time.tm_mon if (cur_time.tm_mon > 9) else f"0{cur_time.tm_mon}"}/{cur_time.tm_year}\
+ {cur_time.tm_hour if (cur_time.tm_hour > 9) else f"0{cur_time.tm_hour}"}:{cur_time.tm_min if (cur_time.tm_min > 9) else f"0{cur_time.tm_min}"}:\
+{cur_time.tm_sec if (cur_time.tm_sec > 9) else f"0{cur_time.tm_sec}"}"""
+
+    return ret
+
+    
+
+print(get_time())
