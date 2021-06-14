2c2e8e48a3dc433668553e1538140f50c7c00d59
Kacper Chołody <cholody.kacper1998@gmail.com>
2021-06-14 20:35:47 +0200

    Added cpu usage monitoring and fonts alignment

--- get_sys_info.py
+++ get_sys_info.py
@@ -1,6 +1,6 @@
 from sys import platform
 from platform import system
-import sys
+from subprocess import check_output
 import time
 
 def check_system_type(expected: str) -> bool:
@@ -12,8 +12,16 @@ def check_system_type(expected: str) -> bool:
 def get_time() -> str:
     cur_time = time.localtime(time.time())
 
-    ret = f"""{cur_time.tm_mday if (cur_time.tm_mday > 9) else f"0{cur_time.tm_mday}"}/{cur_time.tm_mon if (cur_time.tm_mon > 9) else f"0{cur_time.tm_mon}"}/{cur_time.tm_year} \
-{cur_time.tm_hour if (cur_time.tm_hour > 9) else f"0{cur_time.tm_hour}"}:{cur_time.tm_min if (cur_time.tm_min > 9) else f"0{cur_time.tm_min}"}:\
+    ret = \
+f"""{cur_time.tm_hour if (cur_time.tm_hour > 9) else f"0{cur_time.tm_hour}"}:{cur_time.tm_min if (cur_time.tm_min > 9) else f"0{cur_time.tm_min}"}:\
 {cur_time.tm_sec if (cur_time.tm_sec > 9) else f"0{cur_time.tm_sec}"}"""
 
     return ret
+
+def get_cpu_usage() -> str:
+    cmd = "top -bn1 | grep load | cut -d ',' -f 3 | cut -d ':' -f 2"
+    cpu = check_output(cmd, shell = True)
+    cpu.strip()
+    cpu = float(cpu)
+    cpu /= 4.0
+    return str(f"CPU: {round((cpu * 100), 1)}%")
--- show_on_display.py
+++ show_on_display.py
@@ -1,8 +1,7 @@
-from time import sleep
+from time import clock, sleep
 
 from Adafruit_SSD1306 import SSD1306_128_64 as driver
 from PIL import Image, ImageDraw, ImageFont
-
 import get_sys_info 
 
 display = driver(rst=None)
@@ -14,18 +13,45 @@ display.display()
 height = display.height
 width = display.width
 
-screen = Image.new('1', (width, height))
-draw = ImageDraw.Draw(screen)
 
-font = ImageFont.truetype("fonts/OpenSans-Semibold.ttf", 12)
+
+screen = Image.new('1', (width, height))
+
+font_clock = ImageFont.truetype("fonts/OpenSans/OpenSans-Semibold.ttf", 18)
+font_clock_offset_v = int(font_clock.getsize('0')[1] * -0.25)
+font_clock_size = (font_clock.getsize('0')[0], font_clock.getsize('0')[1] - font_clock_offset_v)
+
+font_data = ImageFont.truetype("fonts/OpenSans/OpenSans-Semibold.ttf", 12)
+font_data_offset_v = int(font_data.getsize('0')[1] * -0.25)
+font_data_size = (font_data.getsize('0')[0], font_data.getsize('0')[1] - font_data_offset_v)
+
+print( font_data.getsize("O"))
 
 is_linux = get_sys_info.check_system_type('Linux')
 
-while is_linux:    
-    draw.rectangle((0, 0, width, height), outline = 0, fill = 0)
-    draw.text((0, -2), get_sys_info.get_time(), font=font, fill = 255)
+def update_clock() -> Image:
+    info = get_sys_info.get_time()
+    clock = Image.new('1', (width, font_clock_size[1]))
+    clock_draw = ImageDraw.Draw(clock)
+    clock_draw.rectangle((0, 0, width, font_clock_size[1]), outline = 0, fill = 0)
+    length = 0
+    for letter in info:
+        length += font_clock.getsize(letter)[0]
+    clock_draw.text(((width - length)/2, font_clock_offset_v), info, font=font_clock, fill = 255)
+    return clock
 
-    
+def update_data(function) -> Image:
+    data = Image.new('1', (width, font_data_size[1]))
+    data_draw = ImageDraw.Draw(data)
+    data_draw.rectangle((0, 0, width, font_data_size[1]), outline = 0, fill = 0)
+    data_draw.text((0, font_data_offset_v), function(), font = font_data, fill = 255)
+    return data
+
+while is_linux:
+    clock_screen = update_clock();
+    data_screen = update_data(get_sys_info.get_cpu_usage)
+    screen.paste(clock_screen, (0, 0))
+    screen.paste(data_screen, (0, clock_screen.size[1] + font_clock_offset_v))
     display.image(screen)
     display.display()
     sleep(1)
